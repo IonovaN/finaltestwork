b75a0fa49ee534ae0eb432309bb23d80d66a485a
IonovaNatali <ionovanv@gmail.ru>
2022-09-26 19:09:15 +0300

    Написали функцию

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// сначала функция
+
+void printArr (string[] arr) 
+{
+    int idx=0;
+    Console.Write("[");    
+    while (idx<arr.Length) 
+    {
+        if (idx == arr.Length-1) 
+            Console.Write(arr[idx]);
+        else 
+            Console.Write(arr[idx]+",");
+        idx++;
+    }    
+    Console.Write("]");    
+}
+
