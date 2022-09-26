2a9a3e143509ba60b0da8b8f72cc8a504e80f2d1
IonovaNatali <ionovanv@gmail.ru>
2022-09-26 19:20:38 +0300

    Основная программа

--- Program.cs
+++ Program.cs
@@ -14,4 +14,18 @@ void printArr (string[] arr)
     }    
     Console.Write("]");    
 }
-
+//работа с массивом
+string [] arrIn = {"Hello", "2", "world", ":-)"};
+string [] arrOut = {};
+Console.WriteLine("Исходный массив: ");
+printArr(arrIn);
+int idx = 0;
+while (idx<arrIn.Length) {
+    if (arrIn[idx].Length<=3) {
+        //arrOut = addToArr(arrOut,arrIn[idx]);
+        arrOut=arrOut.Concat(new string[] {arrIn[idx]}).ToArray();
+    }
+    idx++;
+}    
+Console.WriteLine("Обработанный массив:");
+printArr(arrOut);
